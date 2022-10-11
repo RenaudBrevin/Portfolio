bb5bf707251f0be631ccc3d99642ee74ae051d56
RENAUD Brevin <renaudbrevin@gmail.com>
2022-10-11 10:26:39 +0200

    :sparkles: Loading screen and cursor

--- script.js
+++ script.js
@@ -6,3 +6,47 @@
 //     console.log(y);
 //     console.log(b);
 // }
+
+
+// CURSOR 
+
+var cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+
+
+// LOADER
+
+var loader = document.querySelector(".loading");
+
+
+window.addEventListener('load', () => {
+    noLoadScreen();
+})
+
+function waitBro(time)  {
+    return new Promise( resolve => {
+        setTimeout(()=> {resolve('')} ,time );
+    })
+}
+
+async function noLoadScreen(){
+    await waitBro(4800);
+    loader.classList.add("endLoad");
+}
+
+
+
+const titreanime = document.querySelector('.txtCitation');
+
+new Typewriter(titreanime, {
+    loop : false
+    
+})
+
+    .typeString('<span style="color:white">Good things take time...</span>')
+    .start()
+
