5e60aa1752c9faec8810e154bf608d0a146cfde0
RENAUD Brevin <renaudbrevin@gmail.com>
2023-01-03 19:02:04 +0100

    :sparkles: Add new part in index

--- database/connect.php
+++ database/connect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $database=new PDO("mysql:host=localhost;dbname=portfolio;charset=utf8","root","root");
+    $database=new PDO("mysql:host=localhost;dbname=portfolio;charset=utf8","root","");
 } catch(PDOException $error) {
     echo $error;
     die;
